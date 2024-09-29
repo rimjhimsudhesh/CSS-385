70b1d26facb3ee5fc82173f9d77a1c6a2ae88b85
rimjhimsudhesh <rsudhesh75@gmail.com>
2024-09-28 18:01:51 -0700

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Rimjhim Sudhesh on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceRollerTests/DiceRollerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerTests.swift
+//  DiceRollerTests
+//
+//  Created by Rimjhim Sudhesh on 9/28/24.
+//
+
+import Testing
+@testable import DiceRoller
+
+struct DiceRollerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
